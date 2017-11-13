77654ec0832642c5c0f7e5c9b5a6e49465b6a249
Lucas Homer <lucas.homer@gmail.com>
2017-11-13 15:50:48 -0600

    created user input form in main.py

--- main.py
+++ main.py
@@ -3,8 +3,46 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="post">
+            <div>
+                <label for="rot">Rotate by:</label>
+                <input type="text" name="rot" value=0>
+                <p class="error"></p>
+            </div>
+            <textarea type="text" name="text"></textarea>
+            <br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
+
+"""
+
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return form
 
 app.run()
